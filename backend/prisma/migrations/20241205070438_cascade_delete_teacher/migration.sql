4f837b97707fd1c5701448cd72975159940b089c
masood <masoodacheampong@gmail.com>
2024-12-12 21:36:58 +0000

    made changes to the backend's structure

--- backend/prisma/migrations/20241205070438_cascade_delete_teacher/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Record" DROP CONSTRAINT "Record_submitedBy_fkey";
-
--- AddForeignKey
-ALTER TABLE "Record" ADD CONSTRAINT "Record_submitedBy_fkey" FOREIGN KEY ("submitedBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
